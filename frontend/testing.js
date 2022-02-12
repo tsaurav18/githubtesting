7cd7ede7eae0bd198aa745ac282e6134126a5221
tsaurav18 <tsaurav1711@gmail.com>
2022-02-12 14:25:13 +0900

    ss

--- frontend/testing.js
+++ frontend/testing.js
@@ -1,13 +1,12 @@
-// first time when you make new project 
-git init
-git add .
-git commit -m "first commit"
+// first time when you make new project
+// git init
+// git add .
+// git commit -m "first commit"
 
-git remote add origin https://github.com/tsaurav18/zz.git
-git push -u origin master
+// git remote add origin https://github.com/tsaurav18/zz.git
+// git push -u origin master
 
-
-second time 
+// second time
 // git add .
 // git commit -m "message"
 // git push origin master
