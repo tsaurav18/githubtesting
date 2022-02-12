e6dfeb159de1173f049c4293180d08050cec15eb
tsaurav18 <tsaurav1711@gmail.com>
2022-02-12 14:17:05 +0900

    notes" jfkdjdk

--- frontend/testing.js
+++ frontend/testing.js
@@ -1,6 +1,13 @@
+// first time when you make new project 
+git init
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/tsaurav18/zz.git
+git push -u origin master
+
+
+second time 
 // git add .
 // git commit -m "message"
 // git push origin master
-hello;
-dd;
-aa;
